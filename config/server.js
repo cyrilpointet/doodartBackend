319cde0c04e8107ad3125439dddd5fa4242c86fc
CyrilPurjus <c.pointet@purjus.fr>
2020-08-04 08:53:57 +0200

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ec92538d1734efdf05384bc5d5bde54f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f356d9f3-a87d-4a8c-ac4a-9ac7ecea41d2'
+};
