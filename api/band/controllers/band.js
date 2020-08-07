8f2a291c072c739076f90a431ecbb71fcaf40bcb
CyrilPurjus <c.pointet@purjus.fr>
2020-08-07 20:22:08 +0200

    ajout invitation + gestion reponse

--- api/band/controllers/band.js
+++ api/band/controllers/band.js
@@ -7,72 +7,44 @@ const { sanitizeEntity } = require('strapi-utils');
  */
 
 module.exports = {
-  addInvitation: async ctx => {
-    const { bandId, userEmail, isFromBand } = ctx.request.body;
-    const entities = await strapi.services.band.find({ id: bandId });
-    const band = entities[0]
-    let error = null;
-
-    // test members
-    band.members.forEach(member => {
-      if (member.email === userEmail) {
-        error = 'already member'
-      }
-    })
-
-    //test invitations
-    band.invitations.forEach(invitation => {
-      if (invitation.userEmail === userEmail) {
-        error = 'pending invitation'
-      }
-    })
-
-    if (error !== null) {
-      ctx.send({
-        message: error
-      }, 400);
-    } else {
-      band.invitations.push({
-        userEmail,
-        isFromBand
-      })
-      const updatedBand = await strapi.services.band.update(
-        {id: band.id},
-        {invitations: band.invitations}
-      );
-      return sanitizeEntity(updatedBand, { model: strapi.models.band });
-    }
-  },
   processInvitation: async ctx => {
-    let error = null
-    const { bandId, invitationId, value } = ctx.request.body;
-    const band = await strapi.query('band').findOne({id: bandId})
+    const { invitationId, value } = ctx.request.body;
+    const invitationEntity = await strapi.query('invitation').findOne({ id: invitationId });
 
-    // Delete invitation
-    const updateInvitations = band.invitations.filter(elem => elem.id !== invitationId);
-    let updatedBand = await strapi.services.band.update(
-      {id: bandId},
-      {invitations: updateInvitations}
-    );
+    if (invitationEntity === null) {
+      await ctx.send({
+        message: 'invitation not found'
+      }, 400);
+      return
+    }
+
+    const bandEntity = await strapi.query('band').findOne({ id: invitationEntity.band.id });
+    const userEntity = await strapi.query('user', 'users-permissions').findOne({ email: invitationEntity.userEmail });
+
+    await strapi.query('invitation').delete({ id: invitationId });
+
+    if (bandEntity === null || userEntity === null) {
+      await ctx.send({
+        message: 'band or user not found'
+      }, 400);
+      return
+    }
+
+    const members = bandEntity.members.filter(member => member.id === userEntity.id);
+    if (members.length !== 0) {
+      await ctx.send({
+        message: 'member in band'
+      }, 400);
+      return
+    }
 
     if (value === true) {
-      const invitation = await band.invitations.find(elem => elem.id === invitationId);
-      const member = await strapi.query('user', 'users-permissions').findOne({ email: invitation.userEmail });
-      // test member exist
-      if (member === null) {
-        error = 'unknown user';
-        await ctx.send({
-          message: error
-        }, 400);
-        return
-      }
-      band.members.push(member.id)
-      updatedBand = await strapi.services.band.update(
-        {id: bandId},
-        {members: band.members}
-      );
+      bandEntity.members.push(userEntity.id)
     }
-
+    const updatedBand = await strapi.services.band.update(
+      {id: bandEntity.id},
+      {members: bandEntity.members}
+    );
     return sanitizeEntity(updatedBand, { model: strapi.models.band });
   },
 };
---
+++ api/invitation/controllers/invitation.js
@@ -0,0 +1,46 @@
+'use strict';
+const { sanitizeEntity } = require('strapi-utils');
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  create: async ctx => {
+    const { band, userEmail, isFromBand } = ctx.request.body;
+
+    // test band exist
+    const bandEntity = await strapi.services.band.findOne({ id: band });
+    if (bandEntity === null) {
+      await ctx.send({
+        message: 'unknown band'
+      }, 400);
+      return
+    }
+
+    // test member in band
+    const membersWithEmail = bandEntity.members.filter(member => member.email === userEmail);
+    if (membersWithEmail.length !== 0) {
+      await ctx.send({
+        message: 'member in band'
+      }, 400);
+      return
+    }
+    const invitationEntity = await strapi.query('invitation').findOne({ band: band, userEmail: userEmail });
+
+    // test invitation pending
+    if (invitationEntity !== null) {
+       await ctx.send({
+        message: 'invitaion pending'
+      }, 400);
+       return
+    }
+
+    const invitation = await strapi.services.invitation.create({
+      band, userEmail, isFromBand
+    })
+
+    return sanitizeEntity(invitation, { model: strapi.models.invitation })
+  }
+};
